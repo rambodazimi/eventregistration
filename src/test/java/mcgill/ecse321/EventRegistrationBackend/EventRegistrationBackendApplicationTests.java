d8ddee57af39956f3b9d1d567fc823ecdfd490e8
rambodazimi <59399542+rambodazimi@users.noreply.github.com>
2022-01-25 01:27:15 -0500

    Initial commit of the backend application

---
+++ src/test/java/mcgill/ecse321/EventRegistrationBackend/EventRegistrationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mcgill.ecse321.EventRegistrationBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegistrationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
